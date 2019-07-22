8cf45e8038f523a382c55b0090c1056e616bf41e
nibl <nibl@yealink.com>
2019-07-22 15:40:38 +0800

    update base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@
 module.exports = {
     title: 'FrontEnd blog',
     description: 'Just playing around',
+    base: '/frontend-blog/',
     themeConfig: {
         sidebar:[['./guide/foo','测试']],
         sidebarDepth: 3,
