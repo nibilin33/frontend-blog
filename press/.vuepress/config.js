80cec87dfea3eef59c70fb1d8677928e96a261ce
nibl <nibl@yealink.com>
2019-07-25 11:17:37 +0800

    目录梳理

--- press/.vuepress/config.js
+++ press/.vuepress/config.js
@@ -6,8 +6,15 @@ module.exports = {
     themeConfig: {
         sidebar: [
             ['./guide/foo', '内嵌web'],
-            ['./guide/component.md', '组件使用说明'],
-            ['./guide/tool.md', '工具使用说明']
+            ['./guide/component.md', '组件说明'],
+            ['./guide/tool.md', '工具说明'],
+            ['./guide/promto.md', '那些年做过的优化'],
+            ['./guide/knowledge.md', '重要的知识点'],
+            ['./guide/we.md', '小程序'],
+            ['./guide/electron.md', 'Electron'],
+            ['./guide/cooperation.md', '协作的实现'],
+            ['./guide/miniserve.md', '前端微服务'],
+            ['./guide/code.md', '那些年读过的源码'],
         ],
         sidebarDepth: 3,
         displayAllHeaders: true // 默认值：false
---
+++ press/guide/code.md
@@ -0,0 +1,4 @@
+# Vue
+# JQuery
+# Webpack
+# ES
