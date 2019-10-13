e35942b0f24d2c605aa54e41042828f1011076c0
nibilin <819231957@qq.com>
2019-10-13 15:12:02 +0800

    update

--- server.js
+++ server.js
@@ -7,20 +7,23 @@ const app = express();
 const http =  require("http");
 const log4js = require('log4js');
 log4js.configure({
-    appenders: [
-        { type: 'console', category: 'log' }
-    ]
+    appenders: { cheese: { type: 'file', filename: 'cheese.log' } },
+    categories: { default: { appenders: ['cheese'], level: 'error' } }
 });
-const logger = log4js.getLogger('app');
-logger.setLevel('INFO'); // 级别 > INFO 的日志才会被打印
+const logger = log4js.getLogger('cheese');
 app.use(log4js.connectLogger(logger));
 app.use(express.static(path.join(__dirname,'docs')));
 app.use(function(request, response) {
     try {
         response.sendFile(path.resolve('docs/index.html'));
     }catch (e) {
-        console.error(e.toString());
+        logger.error('sendFile error %s', e.toString());
     }
 });
-http.createServer(app).listen(3000);
+http.createServer(app).listen(3000, '0.0.0.0', function () {
+    var host = this.address().address;
+    var port = this.address().port;
+    logger.error('Service listening http at http://%s:%s', host, port);
+});
+
 
