33727d96a70382028118becfc712326f350b1b1d
nibilin33 <nibilin33@gmail.com>
2022-01-20 08:08:31 +0300

    core:update worknote

--- docs/assets/js/15.cb2a0a03.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{410:function(t,e,s){"use strict";s.r(e);var n=s(54),l=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("div",{staticClass:"welcome-bg"})])}),[],!1,null,null,null);e.default=l.exports}}]);
