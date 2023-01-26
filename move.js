058fdc4d76c575c87d9cd00365da031315f8b78e
nibilin33 <nibilin33@gmail.com>
2023-01-26 22:48:20 +0800

    feat: add micro

--- move.js
+++ move.js
@@ -3,3 +3,4 @@ shell.rm('-rf','docs/*');
 shell.cp('-R','press/.vuepress/dist/*','docs');
 shell.cp('-R','press/guide/img/*','docs/assets/img');
 shell.rm('-rf','press/.vuepress/dist');
+shell.cp('-R','ppt/*','docs');
